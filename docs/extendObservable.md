7691deebd8bb5b13657b730c41d6f9026960cb4f
Rain120 <chao1202016@outlook.com>
2020-11-13 19:31:15 +0800

    feat: docs: extendObservable; apis: flow

---
+++ docs/extendObservable.md
@@ -0,0 +1,44 @@
+### extendObservable
+
+`extendObservable(target: A, properties: B, decorators?, options?): A & B`
+
+`extendObservable` 用来向已存在的目标对象添加 `observable` 属性。 属性映射中的所有键值对都会导致目标上的新的 `observable` 属性被初始化为给定值。 属性映射中的任意 `getters` 都会转化成计算属性。
+
+`decorators` 参数用来重载用于指定属性的装饰器, 类似于 `decorate` 和 `observable.object`。
+
+使用 `deep: false` 选项可使得新的属性变成浅的。也就是说, 阻止它们的值自动转换成 `observables`。
+
+```js
+var Person = function (firstName, lastName) {
+  // 在一个新实例上初始化 observable 属性
+  extendObservable(
+    this,
+    {
+      firstName: firstName,
+      lastName: lastName,
+      get fullName() {
+        return this.firstName + ' ' + this.lastName;
+      },
+      setFirstName(firstName) {
+        this.firstName = firstName;
+      }
+    },
+    {
+      setFirstName: action
+    }
+  );
+};
+
+var rain120 = new Person('Rain', '120');
+
+// 向 observable 对象添加 observable 属性
+extendObservable(rain120, {
+  age: 20
+});
+```
+
+注意: `observable.object(object)` 实际上是 `extendObservable({}, object)` 的别名。
+
+注意: 类似于 `extendObservable`, `decorate` 用来为对象引入 `observable` 属性。不同之处在于 `extendObservable` 被设计为直接在目标实例上引入属性, 其中 `decorate` 将它们引入原型; 可以直接将它传递给构造函数(类), 也可以将其作为其他人的原型。
+
+注意: 不能使用 `extendObservable` 来为 `observable` 数组或对象上引入新的属性。
--- src/api/flow.ts
+++ src/api/flow.ts
@@ -2,9 +2,143 @@
  * @Author: Rainy
  * @Date: 2020-10-06 19:53:51
  * @LastEditors: Rainy
- * @LastEditTime: 2020-10-07 15:34:58
+ * @LastEditTime: 2020-11-12 20:53:31
  */
+
+import { resolveSoa } from 'dns';
+import {
+  action,
+  Annotation,
+  isFunction,
+  isUniqueStringLike,
+  noop,
+  storeDecorator
+} from '../internal';
 
 export const FLOW = 'flow';
 
-export const flow = Object.assign(function flow() {});
+let generatorId = 0;
+
+export function FlowCancellationError() {
+  this.message = 'FLOW_CANCELLED';
+}
+
+FlowCancellationError.prototype = Object.create(Error.prototype);
+
+export function isFlowCancellationError(error: Error) {
+  return error instanceof FlowCancellationError;
+}
+
+export type CancellablePromise<T> = Promise<T> & { cancel(): void };
+
+interface Flow extends Annotation, PropertyDecorator {
+  <R, Args extends any[]>(
+    generator: (...args: Args) => Generator<any, R, any> | AsyncGenerator<any, R, any>
+  ): (...args: Args) => CancellablePromise<R>;
+}
+
+export const flow: Flow = Object.assign(
+  function flow(arg1, arg2?) {
+    if (isUniqueStringLike(arg1)) {
+      return storeDecorator(arg1, arg2, 'flow');
+    }
+
+    const generator = arg1;
+    const name = generator.name || '<unnamed flow>';
+
+    const res = function () {
+      const ctx = this;
+      const args = arguments;
+      const runId = ++generatorId;
+      const gen = action(`${name} - runId: ${runId} - init`, generator).apply(ctx, args);
+      let rejector: (error: any) => void;
+      let pendingPromise: CancellablePromise<any> | undefined = undefined;
+
+      const promise = new Promise((resolve, reject) => {
+        let stepId = 0;
+        rejector = reject;
+
+        function onFulfilled(res: any) {
+          pendingPromise = undefined;
+          let ret = null;
+
+          try {
+            ret = action(`${name} - runId: ${runId} - yield ${stepId++}`, gen.next).call(gen, res);
+          } catch (e) {
+            return reject(e);
+          }
+
+          next(ret);
+        }
+
+        function onRejected(err: any) {
+          pendingPromise = undefined;
+          let ret = null;
+
+          try {
+            ret = action(`${name} - runId: ${runId} - yield ${stepId++}`, gen.throw!).call(
+              gen,
+              res
+            );
+          } catch (e) {
+            return reject(e);
+          }
+
+          next(ret);
+        }
+
+        function next(ret: any) {
+          if (isFunction(ret?.then)) {
+            ret.then(next, reject);
+            return;
+          }
+
+          if (ret.done) {
+            return resolve(ret.value);
+          }
+
+          pendingPromise = Promise.resolve(ret.value) as any;
+
+          return pendingPromise!.then(onFulfilled, onRejected);
+        }
+
+        onFulfilled(undefined);
+      }) as any;
+
+      promise.cancel = action(`${name} - runId: ${runId} - cancel`, function () {
+        try {
+          if (pendingPromise) {
+            cancelPromise(pendingPromise);
+          }
+
+          const res = gen.return!(undefined as any);
+          const yieldedPromise = Promise.resolve(res.value);
+          yieldedPromise.then(noop, noop);
+          cancelPromise(pendingPromise);
+          rejector(new FlowCancellationError());
+        } catch (e) {
+          rejector(e);
+        }
+      });
+
+      return promise;
+    };
+
+    res.isMobxFlow = true;
+
+    return res;
+  } as any,
+  {
+    annotationType: 'flow' as const
+  }
+);
+
+export function cancelPromise(promise) {
+  if (isFunction(promise.cancel)) {
+    promise.cancel();
+  }
+}
+
+export function isFlow(fn: any): boolean {
+  return fn?.isMobxFlow === true;
+}
